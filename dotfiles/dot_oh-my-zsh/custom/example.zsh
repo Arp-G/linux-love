bf0786b5b0d4b5d59d7933dfd6aaa2a3626f74e6
Arpan <arpanghoshal3@gmail.com>
2022-12-16 11:09:03 +0530

    Update zsh and add alias

---
+++ dotfiles/dot_oh-my-zsh/custom/example.zsh
@@ -0,0 +1,13 @@
+# You can put files here to add functionality separated per file, which
+# will be ignored by git.
+# Files on the custom/ directory will be automatically loaded by the init
+# script, in alphabetical order.
+
+# For example: add yourself some shortcuts to projects you often work on.
+#
+# brainstormr=~/Projects/development/planetargon/brainstormr
+# cd $brainstormr
+#
+
+# My aliases
+alias iexs="iex -S mix phx.server"
